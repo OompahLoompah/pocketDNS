01171f04c471af361d0a1ccd18b22a6682166d4e
Sean Heuer <kalterplanet@yahoo.com>
2020-04-12 11:03:43 -0700

    Clean up and correct comments

--- pkg/DNS/ResponseFactory.go
+++ pkg/DNS/ResponseFactory.go
@@ -5,7 +5,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
-	//TODO: Remove dependency on gopacket
+	// TODO: Remove dependency on gopacket
 	"github.com/google/gopacket/layers"
 )
 
@@ -14,12 +14,13 @@ type ResponseFactory struct {
 	ARecords map[string]ResourceRecord
 }
 
-//BuildResponse builds and returns a DNS answer.
+// BuildResponse takes a DNS query and builds and returns the correspondibg DNS
+// response.
 func (d *ResponseFactory) BuildResponse(request *layers.DNS) *layers.DNS {
 	replyMess := request // Using the request as a starter for the response
 
-	//Additionals seems to sometimes have an emmpty element. We need to reset
-	//the additionals section as a result
+	// Additionals seems to sometimes have an empty element. We need to reset
+	// the additionals section as a result
 	replyMess.Additionals = nil
 	replyMess.ARCount = 0
 
